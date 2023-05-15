04a8d6505483c77a6cfa9cada2481b8f7c7836eb
RussellDash332 <russellsaerang@gmail.com>
2023-05-15 18:01:06 +0800

    Minor update

--- main.py
+++ main.py
@@ -19,75 +19,84 @@ class WQSession(requests.Session):
         logging.info('Logged in to WQBrain!')
 
     def simulate(self, data):
-        neutralizations = data['neutralizations']
-        decays = data['decays']
-        truncations = data['truncations']
-        delay = data['delay']
-        alphas = data['alphas']
+        try:
+            neutralizations = data['neutralizations']
+            decays = data['decays']
+            truncations = data['truncations']
+            delay = data['delay']
+            alphas = data['alphas']
 
-        logging.info('Creating CSV file')
-        csv_file = f'api_D{delay}_{int(time.time())}.csv'
-        with open(csv_file, 'w', newline='') as f:
-            writer = csv.writer(f)
-            header = [
-                'command', 'passed', 'neutralization', 'decay', 'truncation',
-                'sharpe', 'fitness', 'turnover', 'weight',
-                'subsharpe', 'correlation'
-            ]
-            writer.writerow(header)
+            logging.info('Creating CSV file')
+            csv_file = f'api_D{delay}_{int(time.time())}.csv'
+            with open(csv_file, 'w', newline='') as f:
+                writer = csv.writer(f)
+                header = [
+                    'command', 'passed', 'neutralization', 'decay', 'truncation',
+                    'sharpe', 'fitness', 'turnover', 'weight',
+                    'subsharpe', 'correlation'
+                ]
+                writer.writerow(header)
+                f.flush()
 
-            for neutralization in neutralizations:
-                for decay in decays:
-                    for truncation in truncations:
-                        for alpha in alphas:
-                            logging.info(f'Simulating alpha: {alpha}')
-                            logging.info('Waiting for simulation to end (0%)')
-                            r = self.post('https://api.worldquantbrain.com/simulations', json={
-                                'regular': alpha,
-                                'type': 'REGULAR',
-                                'settings': {
-                                    "nanHandling":"OFF",
-                                    "instrumentType":"EQUITY",
-                                    "delay":delay,
-                                    "universe":"TOP3000",
-                                    "truncation":truncation,
-                                    "unitHandling":"VERIFY",
-                                    "pasteurization":"ON",
-                                    "region":"USA",
-                                    "language":"FASTEXPR",
-                                    "decay":decay,
-                                    "neutralization":neutralization.upper(),
-                                    "visualization":False
-                                }
-                            })
-                            nxt = r.headers['Location']
-                            while True:
-                                time.sleep(1)
-                                r = self.get(nxt).json()
-                                if 'alpha' in r:
-                                    alpha_link = r['alpha']
-                                    break
-                                logging.info(f"Waiting for simulation to end ({int(100*r['progress'])}%)")
-                            r = self.get(f'https://api.worldquantbrain.com/alphas/{alpha_link}').json()
-                            passed = 0
-                            for check in r['is']['checks']:
-                                passed += check['result'] == 'PASS'
-                                if check['name'] == 'CONCENTRATED_WEIGHT':
-                                    weight_check = check['result']
-                                if check['name'] == 'LOW_SUB_UNIVERSE_SHARPE':
-                                    subsharpe = check['value']
-                            row = [
-                                alpha, passed,
-                                neutralization, decay, truncation,
-                                r['is']['sharpe'],
-                                r['is']['fitness'],
-                                100*r['is']['turnover'],
-                                weight_check,
-                                subsharpe,
-                                -1
-                            ]
-                            logging.info(f"{passed}/7 test cases passed!")
-                            writer.writerow(row)
+                for neutralization in neutralizations:
+                    for decay in decays:
+                        for truncation in truncations:
+                            for alpha in alphas:
+                                logging.info(f'Simulating alpha: {alpha}')
+                                logging.info('Waiting for simulation to end (0%)')
+                                r = self.post('https://api.worldquantbrain.com/simulations', json={
+                                    'regular': alpha,
+                                    'type': 'REGULAR',
+                                    'settings': {
+                                        "nanHandling":"OFF",
+                                        "instrumentType":"EQUITY",
+                                        "delay":delay,
+                                        "universe":"TOP3000",
+                                        "truncation":truncation,
+                                        "unitHandling":"VERIFY",
+                                        "pasteurization":"ON",
+                                        "region":"USA",
+                                        "language":"FASTEXPR",
+                                        "decay":decay,
+                                        "neutralization":neutralization.upper(),
+                                        "visualization":False
+                                    }
+                                })
+                                nxt = r.headers['Location']
+                                logging.info(f'Obtained simulation link: {nxt}')
+                                while True:
+                                    time.sleep(2.5)
+                                    r = self.get(nxt).json()
+                                    if 'alpha' in r:
+                                        alpha_link = r['alpha']
+                                        break
+                                    try:    logging.info(f"Waiting for simulation to end ({int(100*r['progress'])}%)")
+                                    except: logging.info(r['message'])
+                                r = self.get(f'https://api.worldquantbrain.com/alphas/{alpha_link}').json()
+                                passed = 0
+                                for check in r['is']['checks']:
+                                    passed += check['result'] == 'PASS'
+                                    if check['name'] == 'CONCENTRATED_WEIGHT':
+                                        weight_check = check['result']
+                                    if check['name'] == 'LOW_SUB_UNIVERSE_SHARPE':
+                                        subsharpe = check['value']
+                                row = [
+                                    alpha, passed,
+                                    neutralization, decay, truncation,
+                                    r['is']['sharpe'],
+                                    r['is']['fitness'],
+                                    100*r['is']['turnover'],
+                                    weight_check,
+                                    subsharpe,
+                                    -1
+                                ]
+                                logging.info(f"{passed}/7 test cases passed!")
+                                writer.writerow(row)
+                                f.flush()
+                                logging.info('Result added to CSV!')
+        except Exception as e:
+            print(e)
+            input('Press enter to quit...')
 
 if __name__ == '__main__':
     WQSession().simulate(DATA)
