587ce349bfd31d7313def34cc413ab28550a66c4
RussellDash332 <russellsaerang@gmail.com>
2023-06-04 14:06:35 +0800

    Update database

--- database.py
+++ database.py
@@ -1,35 +1,30 @@
-# 6
 PRICES = [
     'open', 'high', 'low', 'close',
     'vwap', 'returns'
 ]
 
-# 3
 VOLUMES = [
     'volume', 'adv20', 'cap'
 ]
 
-# 5
 one_OP_one = [
     '+', '-', '*', '/', '^'
 ]
 
-# 7
 OP_one = [
     '', 'rank', 'sigmoid', 'exp',
-    'fraction', 'log', 'log_diff', 'scale'
+    'fraction', 'log', 'log_diff', 'scale',
+    'zscore'
 ]
 
-# 2
 UNARY_OP = ['', '-']
 
-# 15
 TS_OP_1D1P = [
-    'ts_product', 'ts_std_dev',
+    'ts_product', 'ts_std_dev', 'ts_rank',
     'ts_sum', 'ts_entropy', 'ts_av_diff', 'ts_arg_max',
     'ts_decay_linear', 'ts_delay', 'ts_delta', 'ts_ir',
     'ts_max', 'ts_max_diff', 'ts_median', 'ts_min',
-    'ts_min_diff',
+    'ts_min_diff', 'ts_skewness', 'ts_kurtosis'
 
 ]
 
